1cc35385425bf9ba924b620de6d440c2cefaca71
Alan Thomson <alan@projectmccoll.co.uk>
2015-04-26 18:58:51 +0100

    remove core

--- site/application/backend/app-common/Config/core-common.php
+++ site/application/backend/app-common/Config/core-common.php
@@ -42,7 +42,7 @@ require_once(__DIR__ . '/keys.php');
  */
 
 if (Configure::read('ENVIRONMENT') == 'PRODUCTION') {
-	Configure::write('debug', 2);
+	Configure::write('debug', 0);
 } else {
 	Configure::write('debug', 2);
 }
